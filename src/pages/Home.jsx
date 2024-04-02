da3f982b82432bfc4c6b61e778fd40fdbfc619d3
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-02 13:59:40 +0200

    Finished assignment.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import songsData from "../data/songs.json";
+
 // TODO: Import necessary modules and data
 function Home() {
   // TODO: Implement logic to display the list of songs
@@ -6,6 +10,13 @@ function Home() {
       <div>
         <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
         {/* TODO: Display the list of songs with links */}
+        <ul>
+          {songsData.songs.map((song, index) => (
+            <li key={index}>
+              <Link to={`/song/${song.title}`}>{song.title}</Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
--- src/pages/SongInfo.jsx
+++ src/pages/SongInfo.jsx
@@ -1,14 +1,31 @@
 // TODO: Import necessary modules and data
+import React from "react";
+import { useParams } from "react-router-dom";
+import songsData from "../data/songs.json"; // Import the JSON data
 
 // TODO: Destrcutrue props to receive information coming in from the parent comp
 function SongInfo() {
   // TODO: Fetch the song title from the URL and find the corresponding song data and add a conditional to display some simple html if there is no song :)
+  const { songTitle } = useParams();
+
+  // Find the corresponding song data
+  const song = songsData.songs.find((song) => song.title === songTitle);
 
   return (
     <>
       <div>
         <h2>Song Information</h2>
         {/* TODO: Display song details or a "Song not found!" message */}
+        {song ? (
+          <div>
+            <p>Title: {song.title}</p>
+            <p>Artist: {song.artist}</p>
+            <p>Album: {song.album}</p>
+            <p>Year: {song.year}</p>
+          </div>
+        ) : (
+          <p>Song not found!</p>
+        )}
       </div>
     </>
   );
